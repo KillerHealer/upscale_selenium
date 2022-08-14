96e80bc9befdd5a8a220b179c6663fead081c3ef
KillerHealer <noam142196@gmail.com>
2022-08-14 14:59:57 +0300

    second test try 1

--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
+import logging
 import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-
 
 
 def get_driver(url):
@@ -31,4 +31,36 @@ def test_drag_drop():
     driver.quit()
 
 
+def test_iframe():
+    driver = get_driver("http://automationpractice.com/index.php")
+    product = driver.find_element(By.CLASS_NAME, "product-container")
+    product.find_element(By.CLASS_NAME, "icon-eye-open").click()
+    time.sleep(3)
+    iframe = driver.find_element(By.CLASS_NAME, "fancybox-iframe")
+    driver.switch_to.frame(iframe)
+    driver.find_element(By.CSS_SELECTOR, ".box-cart-bottom button").click()
+    time.sleep(3)
+    driver.find_element(By.XPATH, '//*[@id="layer_cart"]/div[1]/div[2]/div[4]/a').click()
+    proceed = driver.find_element(By.ID, "center_column") \
+        .find_element(By.CLASS_NAME, "cart_navigation").find_element(By.CLASS_NAME, "button")
+    actions = ActionChains(driver)
+    actions.move_to_element(proceed).click().perform()
+    proceed = driver.find_element(By.CLASS_NAME, "cart_navigation").find_element(By.TAG_NAME, "button")
+    actions = ActionChains(driver)
+    actions.move_to_element(proceed).click().perform()
+    checkbox = driver.find_element(By.XPATH, '//*[@id="cgv"]')
+    actions = ActionChains(driver)
+    actions.move_to_element(checkbox).click().perform()
+    proceed = driver.find_element(By.NAME, "processCarrier")
+    actions = ActionChains(driver)
+    actions.move_to_element(proceed).click().perform()
+    proceed = driver.find_element(By.CLASS_NAME, "bankwire")
+    actions = ActionChains(driver)
+    actions.move_to_element(proceed).click().perform()
+    proceed = driver.find_element(By.ID, "center_column").find_element(By.TAG_NAME, "button")
+    actions = ActionChains(driver)
+    actions.move_to_element(proceed).click().perform()
+    assert driver.find_element(By.CLASS_NAME, "cheque-indent").text == "Your order on My Store is complete."
+    logging.info("bought the product successfully!")
+    driver.quit()
 
