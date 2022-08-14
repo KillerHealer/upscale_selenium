bdefabbb388b8c6901b8b1f6aa3d2a91d028608a
KillerHealer <noam142196@gmail.com>
2022-08-14 04:42:27 +0300

    getting started

--- tests.py
+++ tests.py
@@ -7,3 +7,10 @@ from selenium.webdriver.common.by import By
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 chrom_driver_path = "D:\pythonProject\chromedriver.exe"
+
+
+def test_drag_and_drop():
+    driver = webdriver.Chrome(chrom_driver_path, chrome_options=chrome_options)
+    driver.maximize_window()
+    driver.get('https://demo.guru99.com/test/drag_drop.html')
+
